a8f415262a4cd11534bb63761c346a738d2697d5
Minbo Bae <baeminbo@google.com>
2024-10-21 19:31:37 -0700

    Add MapState pipeline test in Dataflow Java Legacy Runner without Streaming Engine

---
+++ multimap-timer/run_map.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eu
+trap exit INT
+
+PROJECT=$(gcloud config get project)
+REGION="us-central1"
+MAIN_CLASS=baeminbo.MapStatePipeline
+
+JOB_NAME="map-state"
+mvn compile exec:java \
+  -Dexec.mainClass="$MAIN_CLASS" \
+  -Dexec.args="\
+    --runner=DataflowRunner \
+    --project=$PROJECT \
+    --region=$REGION \
+    --jobName=$JOB_NAME \
+    --experiments=disable_runner_v2 \
+    --workerCacheMb=0 \
+  "
+
+DEBUG_JOB_NAME="map-state-debug"
+DATAFLOW_WORKER_JAR="beam-runners-google-cloud-dataflow-java-legacy-worker-2.59.0.jar"
+mvn compile exec:java \
+  -Dexec.mainClass="$MAIN_CLASS" \
+  -Dexec.args="\
+    --runner=DataflowRunner \
+    --project=$PROJECT \
+    --region=$REGION \
+    --jobName=$DEBUG_JOB_NAME \
+    --experiments=disable_runner_v2 \
+    --workerCacheMb=0 \
+    --dataflowWorkerJar=$DATAFLOW_WORKER_JAR \
+  "
