ec0cfae2dc32f7f2f1a851ef6e4ad8c21b185717
Minbo Bae <baeminbo@google.com>
2025-05-12 05:14:55 -0700

    Add ReshuffleBatchPipeline to Runner v2 shuffle test

---
+++ java-reshuffle/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+trap exit INT
+set eu
+
+PROJECT=$(gcloud config get-value project)
+REGION="us-central1"
+MAIN_CLASS="baeminbo.ReshuffleBatchPipeline"
+
+for count in 1000000 2000000 5000000 10000000
+do
+  for size in 1000 500 200 100 50 20 10
+  do
+  mvn compile exec:java \
+    -Dexec.mainClass="$MAIN_CLASS" \
+    -Dexec.args="\
+      --runner=DataflowRunner \
+      --project=$PROJECT \
+      --region=$REGION \
+      --maxNumWorkers=1 \
+      --experiments=use_runner_v2 \
+      --numberOfWorkerHarnessThreads=48 \
+      --elementCount=${count} \
+      --elementSize=${size} \
+      --latencyMs=3000 \
+      --jobName=reshuffle-v2-${count}-${size} \
+    "
+  done
+done
+
+# --experiments=disable_runner_v2 \
+
